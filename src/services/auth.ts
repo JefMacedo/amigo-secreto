174dc8a07585e03209c9446ab2c716e6bc59076d
Jeferson Macedo <jhef.salles@gmail.com>
2023-12-14 21:32:15 -0300

    Criado a rota de login e token. TODO - Melhorar a autheticação

---
+++ src/services/auth.ts
@@ -0,0 +1,17 @@
+import { getToday } from "../utils/getToday"
+
+export const validadePassword = (password: string) : boolean => {
+    const currentPassword = getToday().split('/').join('')
+    return password === currentPassword;
+}
+
+export const createToken = () => {
+    const currentPassword = getToday().split('/').join('')
+    return `${process.env.DEFAULT_TOKEN}${currentPassword}`
+}
+
+export const validadeToken = (token: string) => {
+    const currentToken = createToken()
+    
+    return token === currentToken
+}
